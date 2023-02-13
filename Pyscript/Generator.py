d19b0d9feba48ea773be0ac7a7e738201bbae27d
SuslikChan <101340685+SuslikChan@users.noreply.github.com>
2023-02-14 01:26:56 +0300

    Базовая версия скрипта
    поправил кодировку комментариев в Solidworks

--- Pyscript/Generator.py
+++ Pyscript/Generator.py
@@ -1,17 +1,18 @@
-from sizes import sizes
+from sizes import sizes as s
+import codecs
 
 files_container = { # Здесь прописываются зависимости для каждого файла
   "Несущая пластина":[
-      ("flange_thread_dim_reducer", sizes["flange_thread_dim_reducer"]),
-      ("flange_thread_depth_reducer", sizes["flange_thread_depth_reducer"]),
-      ("flange_collar_dim_reducer", sizes["flange_collar_dim_reducer"]),
+      ("flange_thread_dim_reducer", s["flange_thread_dim_reducer"]),
+      ("flange_thread_depth_reducer", s["flange_thread_depth_reducer"]),
+      ("flange_collar_dim_reducer", s["flange_collar_dim_reducer"]),
     ]
   
 }
 
 def update_dependencies(): # Функция, создающая новые файлы зависимости
   for file_name in files_container:
-    output = open(f'result/{file_name}.txt','w')
+    output = codecs.open(f'result/{file_name}.txt','wb',encoding='utf-8-sig')
     try:
       data = ""
       for size in files_container[file_name]:
